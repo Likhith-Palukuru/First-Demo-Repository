24127eaa2e6f9418a803b925a64be5eef0d3a6a9
likhith <likhith.palukuru@gmail.com>
2023-03-27 00:04:47 +0530

    demodeployement

--- second_Django_Project/second_Django_Project/settings.py
+++ second_Django_Project/second_Django_Project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-audo(lj20%_e2zpxq+5b3x)a0gzqpec06h&lsgovztqwa63og6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS=[STATIC_DIR,]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
