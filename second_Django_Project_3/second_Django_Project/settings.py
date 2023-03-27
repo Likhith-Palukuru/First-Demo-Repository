7e7fae1c1ac8b515794d5841ad8c67d4faee76f4
Likhith-Palukuru <128956573+Likhith-Palukuru@users.noreply.github.com>
2023-03-27 12:12:58 +0530

    Update settings.py

--- second_Django_Project_3/second_Django_Project/settings.py
+++ second_Django_Project_3/second_Django_Project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-audo(lj20%_e2zpxq+5b3x)a0gzqpec06h&lsgovztqwa63og6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
